81fd0eeeea2680f5c9770711b0a9eb2040da1671
Lesmana <sendycitralesmana@gmail.com>
2023-08-15 17:41:56 +0700

    Integrasi Template AdminLTE

---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,221 @@
+@extends('layouts/main')
+
+@section('title', 'Dashboard')
+
+@section('content')
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>Dashboard</h1>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item active">Dashboard</li>
+                    </ol>
+                </div>
+            </div>
+        </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+
+        <div class="row">
+            <div class="col-md-3">
+                <!-- small box -->
+                <div class="small-box bg-info">
+                    <div class="inner">
+                        <h3> </h3>
+
+                        <p>Desk</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion ion-ios-book-outline"></i>
+                    </div>
+                    <a href="/books" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                </div>
+            </div>
+            <!-- ./col -->
+            <div class="col-md-3">
+                <!-- small box -->
+                <div class="small-box bg-success">
+                    <div class="inner">
+                        <h3> </h3>
+
+                        <p>Desk</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion ion-ios-list-outline"></i>
+                    </div>
+                    <a href="/" class="small-box-footer">More info <i
+                            class="fas fa-arrow-circle-right"></i></a>
+                </div>
+            </div>
+            <!-- ./col -->
+            <div class="col-md-3">
+                <!-- small box -->
+                <div class="small-box bg-warning">
+                    <div class="inner">
+                        <h3> </h3>
+
+                        <p>Desk</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion ion-person-stalker"></i>
+                    </div>
+                    <a href="/" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                </div>
+            </div>
+            <!-- ./col -->
+            <div class="col-md-3">
+                <!-- small box -->
+                <div class="small-box bg-info">
+                    <div class="inner">
+                        <h3> </h3>
+
+                        <p>Desk</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion ion-ios-book-outline"></i>
+                    </div>
+                    <a href="/" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-3">
+                <!-- small box -->
+                <div class="small-box bg-info">
+                    <div class="inner">
+                        <h3> </h3>
+
+                        <p>Desk</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion ion-ios-book-outline"></i>
+                    </div>
+                    <a href="/books" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                </div>
+            </div>
+            <!-- ./col -->
+            <div class="col-md-3">
+                <!-- small box -->
+                <div class="small-box bg-success">
+                    <div class="inner">
+                        <h3> </h3>
+
+                        <p>Desk</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion ion-ios-list-outline"></i>
+                    </div>
+                    <a href="/" class="small-box-footer">More info <i
+                            class="fas fa-arrow-circle-right"></i></a>
+                </div>
+            </div>
+            <!-- ./col -->
+            <div class="col-md-3">
+                <!-- small box -->
+                <div class="small-box bg-warning">
+                    <div class="inner">
+                        <h3> </h3>
+
+                        <p>Desk</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion ion-person-stalker"></i>
+                    </div>
+                    <a href="/" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                </div>
+            </div>
+            <!-- ./col -->
+            <div class="col-md-3">
+                <!-- small box -->
+                <div class="small-box bg-info">
+                    <div class="inner">
+                        <h3> </h3>
+
+                        <p>Desk</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion ion-ios-book-outline"></i>
+                    </div>
+                    <a href="/" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                </div>
+            </div>
+        </div>
+        
+        <div class="row">
+            <div class="col-md-3">
+                <!-- small box -->
+                <div class="small-box bg-info">
+                    <div class="inner">
+                        <h3> </h3>
+
+                        <p>Desk</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion ion-ios-book-outline"></i>
+                    </div>
+                    <a href="/books" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                </div>
+            </div>
+            <!-- ./col -->
+            <div class="col-md-3">
+                <!-- small box -->
+                <div class="small-box bg-success">
+                    <div class="inner">
+                        <h3> </h3>
+
+                        <p>Desk</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion ion-ios-list-outline"></i>
+                    </div>
+                    <a href="/" class="small-box-footer">More info <i
+                            class="fas fa-arrow-circle-right"></i></a>
+                </div>
+            </div>
+            <!-- ./col -->
+            <div class="col-md-3">
+                <!-- small box -->
+                <div class="small-box bg-warning">
+                    <div class="inner">
+                        <h3> </h3>
+
+                        <p>Desk</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion ion-person-stalker"></i>
+                    </div>
+                    <a href="/" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                </div>
+            </div>
+            <!-- ./col -->
+            <div class="col-md-3">
+                <!-- small box -->
+                <div class="small-box bg-info">
+                    <div class="inner">
+                        <h3> </h3>
+
+                        <p>Desk</p>
+                    </div>
+                    <div class="icon">
+                        <i class="ion ion-ios-book-outline"></i>
+                    </div>
+                    <a href="/" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                </div>
+            </div>
+        </div>
+
+    </section>
+    <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+@endsection
